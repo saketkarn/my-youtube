443c2bf02dcf87a52f65bcc90f2789a13a8c0bca
Saket Karn <saketkarn38@gmail.com>
2024-04-23 23:31:24 +0530

    Header and dummy navigation bar

--- src/Components/Head.js
+++ src/Components/Head.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { menu, user_icon, youtube_logo } from '../Utils/Constants'
 
 const Head = () => {
   return (
-    <div>
-      <h1>Head</h1>
-    </div>
+    <div className='grid grid-flow-col p-5 m-2 shadow-lg'>
+        <div className='flex col-span-1 justify-between'>
+            <img className="h-8" alt="menu" src={menu}/>
+            <img className="h-12 mx-2 mt-[-5px]" alt="youtube-logo" src={youtube_logo}/>
+        </div>
+        <div className='col-span-10 px-10'>
+            <input className="w-1/2 border border-gray-400 p-2 rounded-l-full" type="text"/>
+            <button className='border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100'>🔍</button>
+        </div>
+        <div className='col-span-1'>
+            <img className="h-8" alt="user-icon" src={user_icon}/>
+        </div>
+    </div> 
   )
 }
 
---
+++ src/Utils/Constants.js
@@ -0,0 +1,5 @@
+export const menu="https://cdn.iconscout.com/icon/free/png-256/free-hamburger-menu-462145.png?f=webp"
+
+export const youtube_logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3wKeo6bPEBtTqViRPmrATKdsHr2XaAFiEFw&s"
+
+export const user_icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9nPR4_CyTE4xJdV3eDJ3P4rDGs8uvpTEW-qK_-WzU6xfjqg1I1CeyCcPOHztCFtwfbrc&usqp=CAU"
