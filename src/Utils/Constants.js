0d94f9c10acbb3fdd7d8e44d1077e7fd422c9f0f
Saket Karn <saketkarn38@gmail.com>
2024-05-08 20:46:51 +0530

    yt api

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -3,3 +3,8 @@ export const menu="https://cdn.iconscout.com/icon/free/png-256/free-hamburger-me
 export const youtube_logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3wKeo6bPEBtTqViRPmrATKdsHr2XaAFiEFw&s"
 
 export const user_icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9nPR4_CyTE4xJdV3eDJ3P4rDGs8uvpTEW-qK_-WzU6xfjqg1I1CeyCcPOHztCFtwfbrc&usqp=CAU"
+
+const GOOGLE_API_KEY="AIzaSyCaOTB5kPAGhIn2I9WpDaWmt-_nw04PCAU"
+
+export const YOUTUBE_VIDEOS_API= "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2ContentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="+GOOGLE_API_KEY
+
